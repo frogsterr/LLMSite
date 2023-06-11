f5e0b32b2492b345cf3bde9a55a15f63d494951e
Benjy Black <86971030+frogsterr@users.noreply.github.com>
2023-06-10 21:27:38 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,18 +5,39 @@ from PyPDF2 import PdfReader
 # Title
 st.title('sift.ai 🚀')
 
-# File Uploader
-user_pdf = st.file_uploader('Step 1. Upload your .pdf file', type="pdf")
 
-if user_pdf is not None:
-    document = main.pdfToChunks(user_pdf, chunk_size=100, chunk_overlap=20)
-    st.write(document[0].page_content)
+# File type
+file_type = st.selectbox('Step 1. Select File Type', ('PDF', 'CSV', 'Text'))
 
+if file_type == 'PDF':
+    # Upload
+    user_pdf = st.file_uploader('Step 2. Upload your .pdf file', type="pdf")
+
+    if user_pdf is not None:
+        document = main.pdfToChunks(user_pdf, chunk_size=1000, chunk_overlap=200)
+        vectordb_agent = main.vectorStore(texts=document, llm=main.llm)
 
 
 # User Prompt
-prompt = st.text_input('Step 2. Ask away!')
+        prompt = st.text_input('Step 3. Ask away!')
 
-if prompt:
-    response = "why did you ask..."
-    st.write(response)
+        if prompt:
+            try:
+                # Query Style 1.
+
+                response = vectordb_agent.agent_executor.run(prompt)
+
+                # Query Style 2.
+
+                # response = vectordb_agent.qachain.run(prompt) 
+
+                st.write(response)
+            except NameError:
+                st.write("Please upload file")
+
+            with st.expander('Document Similarity Search'):
+                # Find the relevant pages
+                search = vectordb_agent.vectordb.similarity_search_with_score(prompt)
+                # Write out the first
+                st.write(search[0][0].page_content)
+
